af3e4075e6641f5eecf9c2539bbd4f5ccb774d74
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-05-16 17:01:25 +0800

    [GREEN] Implement Note Validation

--- src/functions/validateNote.js
+++ src/functions/validateNote.js
@@ -1,3 +1,9 @@
 export default function validateNote(note) {
-    return "Not Implemented";
+    if (note.title.trim() === "") {
+        return false;
+    }
+    if (note.content.length > 1000) {
+        return false;
+    }
+    return true;
 }
