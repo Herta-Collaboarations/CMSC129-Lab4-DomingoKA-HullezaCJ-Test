74c2573eb7c710bae596c50dfdfe4427093c836c
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-05-16 22:12:45 +0800

    [Green] Impelment integration layer for Reading a Note from as-if DB

--- src/functions/getNote.js
+++ src/functions/getNote.js
@@ -1,2 +1,17 @@
 export default async function getNote(id) {
+    try {
+        const endpoint = `http://localhost:3000/notes/${id}`;
+        const request = {
+            "method": "GET",
+        };
+        const response = await fetch(endpoint, request);
+        const result = await response.json();
+        console.log(response.status);
+        return {"status": response.status, "body": result};    
+    } catch(e) {
+        return { 
+            "status": 500, 
+            "body": { "error": "Unable to retrieve notes at this time" } 
+        };
+    }
 }
