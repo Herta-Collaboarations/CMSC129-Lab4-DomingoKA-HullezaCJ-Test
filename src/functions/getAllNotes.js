253cf4db86b8c21495288f558ad22bff4ebc6c96
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-05-17 08:54:18 +0800

    [DOCS] Integration test results

--- src/functions/getAllNotes.js
+++ src/functions/getAllNotes.js
@@ -1,17 +1,17 @@
 export default async function getAllNotes() {
-    // try {
-    //     const endpoint = `http://localhost:3000/notes/`;
-    //     const request = {
-    //         "method": "GET",
-    //     };
-    //     const response = await fetch(endpoint, request);
-    //     const result = await response.json();
-    //     return {"status": response.status, "body": result};    
-    // } catch(e) {
-    //     return { 
-    //         "status": 500, 
-    //         "body": { "error": "Unable to retrieve notes at this time" } 
-    //     };
-    // }
+    try {
+        const endpoint = `http://localhost:3000/notes/`;
+        const request = {
+            "method": "GET",
+        };
+        const response = await fetch(endpoint, request);
+        const result = await response.json();
+        return {"status": response.status, "body": result};    
+    } catch(e) {
+        return { 
+            "status": 500, 
+            "body": { "error": "Unable to retrieve notes at this time" } 
+        };
+    }
 
 }
